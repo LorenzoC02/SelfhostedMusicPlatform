ce60ca42afba0cf98069665f3b959c2ea5d3e6c6
LorenzoC02 <lorenzo.cocchi2002@gmail.com>
2025-12-23 10:15:16 +0100

    jwt implement

--- auth-svc/src/main/java/com/selfhost/auth/auth_svc/User.java
+++ auth-svc/src/main/java/com/selfhost/auth/auth_svc/User.java
@@ -1,1 +1,48 @@
+package com.selfhost.auth.auth_svc;
 //https://freedium.cfd/@max.difranco/building-a-user-registration-and-jwt-authentication-service-with-spring-boot-3-66cf76233204
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private Instant updatedAt;
+}
